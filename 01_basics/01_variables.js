dfa28d832200faa2e6eef4d412c0de847cf11c43
Abhijeet Kumar <abhjeets61@gmail.com>
2025-06-13 15:02:40 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=1223445
+let accountEmail="Abhijeet@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
+// accountId=2345 // not allowed to change const it will give error
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"  //treat all js code as newer version.Now u can't use use no strict.....
+
+//alert(3+3)    //we are using node js, not browser
+
+//code Readiblity should be high.
+
+let name="abhijeet"
+let age=18
+let isLoggedIn=false
+
+// number=> 2^53
+// bigint
+//string=> " "
+// boolean=> true/false
+//null => standalone value
+//undefined =>value not assigned yet
+//symbol => for finding uniqueness
+
+//object
+
+console.log(typeof"Hitesh"); // console.log(typeof("Hitesh"))
+console.log(typeof null);
+console.log(typeof undefined);
