3d92ba03ddea52c22cc28855e5aee1f11b455f06
Abhijeet Kumar <abhjeets61@gmail.com>
2025-07-05 15:45:23 +0000

    memory concept done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,7 +2,7 @@
 //JavaScript is dynamically typed.Dynamically typed means you don't need to declare variable types. The type is determined at runtime based on the value assigned.
 //7 Types: String,NumberBoolean,null,undefined,Symbol,BigInt
 
-const score=100
+/*const score=100
 const scoreValue=100.3
 const isLoggedIn=false
 const outsideTemp=null
@@ -11,7 +11,29 @@ const id=Symbol('123')
 const anotherId=Symbol('123')
 console.log(id===anotherId);
 
+*/
 
 //Reference(Non primitive)
 
 // Array,Objects,Functions
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitive)=> copy,Heap(Non-primitve)=>Refernce
+
+let youtubeName="abhijeetkumardotcome"
+let anotherName=youtubeName
+anotherName="chaiAurCode"
+console.log(youtubeName);
+console.log(anotherName);
+
+let user1={
+    email:"user@google.com",       //key value pair always separated by comma except the last one
+    upi:"user@ybl"
+}
+let user2=user1
+user2.email="abhijeet@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
