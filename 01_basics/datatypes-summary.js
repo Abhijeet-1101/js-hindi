4d2ed9333928703ace6a689a4e8e9aadb67d3427
Abhijeet Kumar <abhjeets61@gmail.com>
2025-07-05 15:04:22 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+//primitive
+//JavaScript is dynamically typed.Dynamically typed means you don't need to declare variable types. The type is determined at runtime based on the value assigned.
+//7 Types: String,NumberBoolean,null,undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail; //undefined automatically
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+//Reference(Non primitive)
+
+// Array,Objects,Functions
